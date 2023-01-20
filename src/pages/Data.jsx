ab2ddb222680eb954d9b59090185f9ad138d7aa8
Pabl0Parra <pabsblue16@gmail.com>
2023-01-20 16:54:55 +0100

    fetch houses with redux on Data page

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Text } from '../components/atoms'
 import { Body } from '../components/layout'
+import { getHouses } from '../store/houses.slice'
 
 function Data() {
-  return <Body>Data</Body>
+  const dispatch = useDispatch()
+  const houses = useSelector((state) => state.houses.houses)
+  const { allIds, byId } = houses
+
+  useEffect(() => {
+    dispatch(getHouses())
+  }, [dispatch])
+
+  return (
+    <Body>
+      {allIds.map((id) => (
+        <Text>{byId[id].title}</Text>
+      ))}
+    </Body>
+  )
 }
 
 export default Data
