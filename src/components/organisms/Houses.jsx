cc7e02f3ac2e8e3344fd1f0d3e6870a4974ff593
Pabl0Parra <pabsblue16@gmail.com>
2023-01-20 18:07:14 +0100

    render all houses at once through redux

--- src/components/organisms/Houses.jsx
+++ src/components/organisms/Houses.jsx
@@ -1,58 +1,47 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
 import { Button } from '../atoms'
 import { HouseCard } from '../molecules'
-import { useFetch } from '../../hooks'
 import { FlexBox, Grid } from '../../styles'
-import { urls } from '../../constants'
+import { getHouses } from '../../store/houses.slice'
 
 const HousesStyled = styled(FlexBox)``
 
 function Houses() {
-  const [houses, setHouses] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const perPage = 9
-  const { data, loading, isError, isSuccess } = useFetch(
-    `${urls.houses}?per_page=${perPage}`,
-  )
+  const dispatch = useDispatch()
+  const houses = useSelector((state) => state.houses.houses)
+  const { allIds, byId } = houses
+  const [renderedHouses, setRenderedHouses] = useState(9)
 
   useEffect(() => {
-    if (!data) return
-    setHouses(data.slice(0, 9))
-  }, [data])
+    dispatch(getHouses())
+  }, [dispatch])
 
   const handleLoadMore = () => {
-    setHouses([
-      ...houses,
-      ...data.slice(currentPage * perPage, currentPage * perPage + 9),
-    ])
-    setCurrentPage(currentPage + 1)
+    setRenderedHouses(renderedHouses + 9)
   }
 
   return (
     <HousesStyled>
-      {loading && <div>Loading...</div>}
-      {isError && <div>Error</div>}
-      {isSuccess && (
-        <Grid gridGap="32px">
-          {houses.map((house) => (
-            <HouseCard
-              key={house.id}
-              title={house.title}
-              price={`${house.price}€`}
-              img={house.image}
-              link=""
-            />
-          ))}
-        </Grid>
-      )}
-      {houses.length < data.length && (
-        <FlexBox align="center">
-          <Button style={{ marginTop: '2rem' }} onClick={handleLoadMore}>
-            Load more
-          </Button>
-        </FlexBox>
-      )}
+      <Grid gridGap="32px">
+        {}
+        {allIds.map((id) => (
+          <HouseCard
+            key={byId[id].id}
+            title={byId[id].title}
+            price={`${byId[id].price}€`}
+            img={byId[id].image}
+            link=""
+          />
+        ))}
+      </Grid>
+
+      <FlexBox align="center">
+        <Button style={{ marginTop: '2rem' }} onClick={handleLoadMore}>
+          Load more
+        </Button>
+      </FlexBox>
     </HousesStyled>
   )
 }
